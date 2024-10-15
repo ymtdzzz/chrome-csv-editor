14b676b3f1f613d8d3ad15fe1d1aa27e5270f00e
Y.Matsuda <ymatsuda.zck@gmail.com>
2024-10-15 18:56:21 +0900

    Add privacy policy

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -29,7 +29,7 @@ const manifest = deepmerge(
     version: packageJson.version,
     description: '__MSG_extensionDescription__',
     host_permissions: ['<all_urls>'],
-    permissions: ['storage', 'scripting', 'tabs', 'notifications'],
+    permissions: ['storage', 'scripting', 'tabs'],
     options_page: 'options/index.html',
     background: {
       service_worker: 'background.iife.js',
